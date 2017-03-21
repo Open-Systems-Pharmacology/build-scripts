b7bab97d27a2ca1fe8010be001335d7b602545c6
rwmcintosh <rob@robertmcintosh.ca>
2017-03-21 14:25:18 -0400

    remove puts from script

--- coverage.rb
+++ coverage.rb
@@ -7,8 +7,6 @@ task :cover, [:filter] do |task, args|
 
 	targetArgs = testProjects.join(" ")
 
-	puts args.filter
-
 	Utils.run_cmd(openCover, ["-register:user", "-target:nunit3-console.exe", "-targetargs:#{targetArgs}", "-output:OpenCover.xml", "-filter:#{args.filter}", "-excludebyfile:*.Designer.cs"])
 
 	Utils.run_cmd(coveralls, ["--opencover", "-i", "OpenCover.xml", "--useRelativePaths", "--repoToken", ENV['COVERALLS_REPO_TOKEN'], "--commitId", ENV['APPVEYOR_REPO_COMMIT'], "--commitBranch", ENV['APPVEYOR_REPO_BRANCH'], "--commitAuthor", ENV['APPVEYOR_REPO_COMMIT_AUTHOR'], "--commitEmail", ENV['APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL'], "--commitMessage", ENV['APPVEYOR_REPO_COMMIT_MESSAGE'], "--jobId", ENV['APPVEYOR_BUILD_NUMBER'], "--serviceName", "appveyor"])
