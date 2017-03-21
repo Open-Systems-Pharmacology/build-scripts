9482f13cdf895c752e63b88434270395c9aecdea
Robert McIntosh <rob@robertmcintosh.ca>
2017-03-21 15:38:55 -0400

    Fixes #1 Calculate and upload coverage reports to coveralls.io #1 (#2)
    * adding coverage script
    
    * force repoToken in command to coveralls
    
    * uncomment test run
    
    * really expand environment variables
    
    * exclude designer files
    
    * remove puts from script
    
    * fixed indentation

---
+++ coverage.rb
@@ -0,0 +1,12 @@
+require_relative 'utils'
+
+task :cover, [:filter] do |task, args|
+  coveralls = Dir.glob("packages/coveralls.net.*/tools/csmacnz.coveralls.exe").first
+  testProjects = Dir.glob("tests/*.Tests/*.Tests.csproj")
+  openCover = Dir.glob("packages/OpenCover.*/tools/OpenCover.Console.exe").first
+
+  targetArgs = testProjects.join(" ")
+
+  Utils.run_cmd(openCover, ["-register:user", "-target:nunit3-console.exe", "-targetargs:#{targetArgs}", "-output:OpenCover.xml", "-filter:#{args.filter}", "-excludebyfile:*.Designer.cs"])
+  Utils.run_cmd(coveralls, ["--opencover", "-i", "OpenCover.xml", "--useRelativePaths", "--repoToken", ENV['COVERALLS_REPO_TOKEN'], "--commitId", ENV['APPVEYOR_REPO_COMMIT'], "--commitBranch", ENV['APPVEYOR_REPO_BRANCH'], "--commitAuthor", ENV['APPVEYOR_REPO_COMMIT_AUTHOR'], "--commitEmail", ENV['APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL'], "--commitMessage", ENV['APPVEYOR_REPO_COMMIT_MESSAGE'], "--jobId", ENV['APPVEYOR_BUILD_NUMBER'], "--serviceName", "appveyor"])
+end
