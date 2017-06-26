dfdcd585740a12f6579c0c913bf6939192ad9b93
Michael Sevestre <michael@design2code.ca>
2017-06-26 10:39:27 -0400

    Update setup script to support portable setup

--- setup.rb
+++ setup.rb
@@ -4,7 +4,7 @@ require_relative 'wix'
 
 namespace :setup do
 	desc "Performs the required steps to create a setup"
-	task :create, [:src_dir, :setup_dir, :deploy_dir, :product_name, :product_version, :harvest_ignored_files, :suite_name, :setup_files, :solution_dir, :manufacturer] do |task, args|
+	task :create, [:src_dir, :setup_dir,  :product_name, :product_version, :harvest_ignored_files, :suite_name, :setup_files, :solution_dir, :manufacturer] do |task, args|
 		@deploy_dir =  File.join(args.setup_dir, 'deploy');
 		@harvest_dir = File.join(@deploy_dir, 'harvest');
 		@product_version	= args.product_version
@@ -12,7 +12,7 @@ namespace :setup do
 		@manufacturer = args.manufacturer
 		@suite_name = args.suite_name
 		@harvest_ignored_files = args.harvest_ignored_files || []
-		@setup_files = args.setup_files || []
+		setup_files = args.setup_files || []
 		@solution_dir = args.solution_dir;
 
 		create_deploy_dir
@@ -20,7 +20,7 @@ namespace :setup do
 		
 		harvest_app				
 
-		copy_setup_files args.setup_files					
+		copy_setup_files setup_files, @deploy_dir				
 		Rake::Task['setup:create_setup'].invoke
 	end
 
@@ -36,10 +36,9 @@ namespace :setup do
 		@variables[:ProductName] =@product_name
 		@variables[:ProductVersion] = @product_version
 		@variables[:Manufacturer] = @manufacturer
-		release_version_split= @product_version.split('.')
-		release_version = "#{release_version_split[0]}.#{release_version_split[1]}"
+		release_version, *	 = versions_from(@product_version)
 		@variables[:ProductReleaseVersion] = release_version	
-		@variables[:ProductFullName] = "#{@product_name} #{release_version}"
+		@variables[:ProductFullName] = product_full_name_from(@product_name, release_version)
 	end
 
 	desc "Runs the candle executable as first step of the setup process"
@@ -59,7 +58,47 @@ namespace :setup do
 		Utils.run_cmd(Wix.light, all_wixobj + all_options)
 	end
 
-private
+	desc "Creates a portable	setup"
+	task :create_portable, [:solution_dir, :src_dir, :setup_dir, :setup_files, :setup_folders, :product_name, :product_version] do |task, args|
+		@solution_dir = args.solution_dir;
+		release_version, full_version	 = versions_from(args.product_version)
+		product_full_name = product_full_name_from(args.product_name, full_version)
+		setup_files = args.setup_files || []
+		setup_folders = args.setup_folders || []
+	
+		portable_dir = File.join(args.setup_dir, product_full_name)
+		archive_path = File.join(args.setup_dir, 'portable-setup.zip')
+
+		create_portable_dir portable_dir
+		
+		copy_src_files_to_portable_dir args.src_dir, portable_dir	
+		copy_setup_files setup_files, portable_dir
+		copy_setup_folders setup_folders, portable_dir
+
+		archive_portable_dir archive_path, portable_dir	
+	end
+
+	private
+	def archive_portable_dir(archive_path, portable_dir)	
+		command_line = %W[a #{archive_path} #{portable_dir}]
+		zip command_line
+	end
+
+	def product_full_name_from(product_name, release_version)
+		"#{product_name} #{release_version}"
+	end
+
+	def versions_from(product_version)
+		product_version_split = product_version.split('.')
+		release_version = "#{product_version_split[0]}.#{product_version_split[1]}"
+		full_version = "#{release_version}.#{product_version_split[2]}"
+		return release_version	, full_version
+	end
+
+	def zip(command_line)
+	  Utils.run_cmd('7z', command_line) 
+	end
+
 	def harvest_app
 		@harvest_ignored_files.each do |file|
 			FileUtils.rm File.join(@harvest_dir, file)
@@ -68,10 +107,20 @@ private
 		Rake::Task[:heat].execute  OpenStruct.new(source_directory: @harvest_dir, component_name: 'App', output_dir:  @deploy_dir)
 	end
 
+	def create_portable_dir(portable_dir)
+		FileUtils.rm_rf  portable_dir
+		FileUtils.mkdir_p portable_dir		
+	end
+
 	def create_deploy_dir
 		FileUtils.rm_rf  @deploy_dir
 		FileUtils.mkdir_p @deploy_dir
 		FileUtils.mkdir_p @harvest_dir		  
+	end
+
+	def copy_src_files_to_portable_dir(src_dir, portable_dir)
+		src_files = File.join(src_dir, '*.*')
+		copy_to_target_dir src_files, portable_dir, %w[pdb xml]
 	end
 
 	def copy_src_files(src_dir)
@@ -80,9 +129,33 @@ private
 		copy_to_target_dir src_files, @harvest_dir, %w[pdb xml]
 	end
 
-	def copy_setup_files(setup_files)
+	def copy_setup_files(setup_files, target_dir)
 		setup_files.each do |file|
-			copy_to_deploy_dir File.join(@solution_dir, file)
+			copy_to_target_dir File.join(@solution_dir, file), target_dir
+		end
+	end
+
+	def copy_setup_folders(setup_folders, target_dir)
+		setup_folders.each do |folder|
+			folder_entries = folder.split('/');
+			
+			#first entry in path that is not a star. We will copy structure from there
+			folder_base = folder_entries[folder_entries.rindex{ |x| !x.include? "*" }]
+			
+			copy_to_target_dir_preserve folder, target_dir, folder_base
+		end
+	end
+
+	def copy_to_target_dir_preserve(source, target_dir, base_dir)
+		Dir.glob	source do |file|
+			dir_entries = File.dirname(file).split('/')
+			base_index = dir_entries.rindex{ |x| x == base_dir }
+				
+			#last element in the path containing the base. We want to keep everything AFTER that
+			dir_entries = dir_entries.drop(base_index + 1)
+			dest = File.join(target_dir, dir_entries)
+			FileUtils.mkdir_p dest
+			FileUtils.copy_file file, File.join(dest, File.basename(file)), verbose: true
 		end
 	end
 
@@ -97,6 +170,7 @@ private
 	end
 
 	def file_should_be_ignored(file, ignored_extensions)
-		ignored_extensions.any? { |ext| file.include? ext  }
+		ignored_extensions.any? { |ext| file.include? ext }
 	end
 end
+
